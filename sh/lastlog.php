ac88e3c8d2fe455957f9538d10af3f3723503dcf
inflammable <root@raronastwo.raromansion>
2014-05-06 21:03:58 +1200

    Change lastlog.php to read multiple logins from last with full hostname.

--- sh/lastlog.php
+++ sh/lastlog.php
@@ -1,14 +1,14 @@
 <?php
 
 exec(
-    '/usr/bin/lastlog --time 365 |' .
-    ' /usr/bin/awk \'{print $1","$3","$4" "$5" "$6" "$7" "$8}\'',
+    '/usr/bin/last -w -n6 |' .
+    ' /usr/bin/awk \'!/^wtmp|^$/ {print $1","$3","$4" "$5" "$6" "$7}\'',
     $users
 );
 $result = array();
 # ignore the first line of column names
-for ($i = 1; $i < count($users); ++$i) {
-    $result[$i-1] = explode(",", $users[$i]);
+for ($i = 0; $i < count($users); ++$i) {
+    $result[$i] = explode(",", $users[$i]);
 }
 
 header('Content-Type: application/json; charset=UTF-8');
